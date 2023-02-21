73ad36c51fb37d551cd9c3bc19a7871201dadb94
Enzo.C <Enzocapi@hotmail.com>
2023-02-21 16:14:16 +0100

    try fix CORS ORIGIN

--- controllers/back/user.controller.php
+++ controllers/back/user.controller.php
@@ -10,16 +10,8 @@ class UserController
 
     public function getUserInfo()
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            header("Access-Control-Allow-Origin: *");
-            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            header("Access-Control-Allow-Credentials: true");
-            header("Content-Type: application/json");
-            header('Access-Control-Max-Age: 86400');
-            http_response_code(200);
-            exit();
-        }
+
+
 
         // Vérifier que l'utilisateur est authentifié
         $authController = new AuthController();
