8a15ebb2c4745bb2e06226f3bc217ba9b2ddf5c6
Enzo.C <Enzocapi@hotmail.com>
2023-02-26 15:44:44 +0100

    Reservation view and modify OK/ CREATE is on developpement

---
+++ controllers/back/reservations.controller.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once "./controllers/back/Security.class.php";
+require_once "./models/back/reservations.manager.php";
+require_once "./controllers/back/utile.php";
+
+class reservationsController
+{
+    private $reservationsManager;
+
+    public function __construct()
+    {
+        $this->reservationsManager = new reservationsManager();
+    }
+    public function visualisation()
+    {
+        if (security::verifAccessSession()) {
+            $reservations = $this->reservationsManager->getreservations();
+            // print_r($reservations);
+            require_once "views/reservationsVisualisation.php";
+        } else {
+            throw new Exception("Vous n'avez pas les droits.");
+        }
+    }
+    public function delete()
+    {
+        if (security::verifAccessSession()) {
+            $_SESSION['alert'] = [
+                "message" => "Le plat est supprimé",
+                "type" => "alert-success"
+            ];
+
+            $this->reservationsManager->deleteDBreservation((int) security::secureHTML($_POST['reservation_id']));
+            header('Location: ' . URL . 'back/reservations/visualisation');
+        } else {
+            throw new Exception("Vous n'avez pas les droits.");
+        }
+    }
+
+    public function modification()
+    {
+        if (
+            isset($_POST['reservation_id']) &&
+            isset($_POST['client_id']) &&
+            isset($_POST['date']) &&
+            isset($_POST['time']) &&
+            isset($_POST['comments'])
+        ) {
+            $reservation_id = intval($_POST['reservation_id']);
+            $client_id = intval($_POST['client_id']);
+            $date = filter_var($_POST['date']);
+            $time = filter_var($_POST['time']);
+            $comments = filter_var($_POST['comments']);
+
+
+            if (!$reservation_id) {
+                throw new Exception("Les données envoyées sont incorrectes : reservation_id doit être un entier valide.");
+            } else if (!$reservation_id || !$client_id) {
+                throw new Exception("Les données envoyées sont incorrectes : client_id ne peut pas être vide.");
+            } else {
+                $this->reservationsManager->updatereservation($reservation_id, $client_id, $date, $time, $comments);
+                $_SESSION['alert'] = [
+                    "message" => "La réservation à bien été modifiée",
+                    "type" => "alert-success"
+                ];
+                header("Location: " . URL . "back/reservations/visualisation");
+            }
+        } else {
+            throw new Exception("Les données envoyées sont incorrectes : reservation_id, client_id, date, time, comments doivent être définis.");
+        }
+    }
+
+    public function creationTemplate()
+    {
+        if (security::verifAccessSession()) {
+            require_once "views/reservationCreation.view.php";
+        } else {
+            throw new Exception("Vous n'avez pas les droits.");
+        }
+    }
+
+    public function creationValidation()
+    {
+        if (security::verifAccessSession()) {
+            $reservation_id = security::secureHTML($_POST['reservation_title']);
+            $client_id = security::secureHTML($_POST['client_id']);
+            $date = "";
+            $time = "";
+            $comments = "";
+
+            $reservation_id =  $this->reservationsManager->createReservation($reservation_id, $client_id, $date, $time, $comments);
+
+            $_SESSION['alert'] = [
+                "message" => "Le plat à été créé avec l'identifiant :" . $reservation_id,
+                "type" => "alert-success"
+            ];
+            header("Location: " . URL . "back/reservations/visualisation");
+            // var_dump($_POST);
+        } else {
+            throw new Exception("Vous n'avez pas les droits.");
+        }
+    }
+}
---
+++ views/reservationsVisualisation.php
@@ -0,0 +1,61 @@
+<?php ob_start(); ?>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">ID Client</th>
+            <th scope="col">Date</th>
+            <th scope="col">Heure</th>
+            <th scope="col">Commentaire</th>
+            <th scope="col" colspan="2">actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($reservations as $reservation) : ?>
+            <?php if (isset($_POST['reservation_id']) && $_POST['reservation_id'] == $reservation['reservation_id']) : ?>
+                <form method="POST" action="<?= URL ?>back/reservations/validationModification">
+                    <tr>
+                        <td><?= $reservation['reservation_id'] ?></td>
+                        <td><?= $reservation['client_id'] ?></td>
+                        <!-- Hidden input containes client's ID -->
+                        <input type="hidden" name="client_id" value="<?= $reservation['client_id'] ?>" />
+                        <td><input type="text" name="date" class="form-control" value="<?= $reservation['date'] ?>" /></td>
+                        <td><input type="text" name="time" class="form-control" value="<?= $reservation['time'] ?>" /></td>
+                        <td><textarea name='comments' class="form-control" rows="3"><?= $reservation['comments'] ?></textarea></td>
+
+                        <td colspan="2">
+                            <input type="hidden" name="reservation_id" value="<?= $reservation['reservation_id'] ?>" />
+                            <button class="btn btn-primary" type="submit">Valider</button>
+                        </td>
+                    </tr>
+                </form>
+            <?php else : ?>
+                <tr>
+                    <td><?= $reservation['reservation_id'] ?></td>
+                    <td><?= $reservation['client_id'] ?></td>
+                    <td><?= $reservation['date'] ?></td>
+                    <td><?= $reservation['time'] ?></td>
+                    <td><?= $reservation['comments'] ?></td>
+                    <td>
+                        <form method="post" action="">
+                            <input type="hidden" name="reservation_id" value="<?= $reservation['reservation_id'] ?>" />
+                            <button class="btn btn-warning" type="submit">Modifier</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form method="post" action="<?= URL ?>back/reservations/validationDelete" onSubmit="return confirm('Voulez-vous vraiment supprimer ?');">
+                            <input type="hidden" name="reservation_id" value="<?= $reservation['reservation_id'] ?>" />
+                            <button class="btn btn-danger" type="submit">Supprimer</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php
+$content = ob_get_clean();
+$title = "Administration des réservations";
+require "views/commons/template.php";
+?>
